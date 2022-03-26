870eb4361ce32e2ff3c8e8f7aae8c4f2e2cbc05c
Pimexworld <101183239+Pimexworld@users.noreply.github.com>
2022-03-26 16:04:30 +0100

    Add files via upload

---
+++ praise7.js
@@ -0,0 +1,55 @@
+console.log('Hello world');
+let score;
+
+score=10+2;// i love this code
+console.log(score);
+var myName= "Praise";
+var a=2;
+var b=4;
+var c="i am a";
+console.log(c+"string");
+
+ a= a+34;
+ b=b+4;
+c=a+b;
+console.log(c+"string");
+var firstLetterOfFirstName="";
+var firstName="Joy";
+ firstLetterOfFirstName=firstName[0];
+console.log(firstLetterOfFirstName);
+var myArray=["Praise",  1];
+console.log(myArray);
+
+var myArray=[12, 13, 12];
+myArray[2]=34;
+console.log(myArray);
+
+var myArray=["cat","dog" ,"blossom"];
+myArray.shift();
+myArray.unshift("joy");
+
+console.log(myArray);
+
+function ourResuableFunction(){console.log("Praise is nice");}
+ourResuableFunction();
+ourResuableFunction();
+ourResuableFunction();
+
+function functionWithPraise(a,b){console.log(a-b);}
+functionWithPraise(18, 5);
+
+var myPraise={
+    "name":"Happy man",
+    "age":"23"}
+   
+    console.log(myPraise);
+
+    function trueOrFalse(wasThatTrue){
+        if(wasThatTrue){
+            return "Yes, that was true"
+        }
+        return "No, that was false"
+    }
+    console.log(trueOrFalse(false));
+    
+
